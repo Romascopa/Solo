cc04d0f943596ce665c3a04f674a0cbda5814187
Lance Robbins <riotchild25@gmail.com>
2018-02-27 01:54:42 -0500

    preloader done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ===================================================
+                        Preloader
+=================================================== */
+$(window).on('load', function() { // makes sure whole site is loaded
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut();
+});
