daea4972d84c3d2ba566c7dabd4148d2822a5ec7
lrobbins <riotchild25@gmail.com>
2018-03-06 01:25:43 -0500

    team section started

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ $(window).on('load', function() { // makes sure whole site is loaded
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+/* ===================================================
+                        Team Members
+=================================================== */
+$(function() {
+    $('#team-members').owlCarousel({
+        items:2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    });
+});
